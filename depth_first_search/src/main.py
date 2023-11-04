2cab6089d7e0e03a7f54a8a4c980fb74858cdef8
joana <joanaalvesm@gmail.com>
2023-11-04 15:10:30 -0300

    Modified to read the graph from local file

--- depth_first_search/src/main.py
+++ depth_first_search/src/main.py
@@ -1,28 +1,27 @@
-# source: https://www.scaler.com/topics/dfs-python/
-
 def dfs(node, graph, visited, component):
     component.append(node)  # Store answer
     visited[node] = True  # Mark visited
 
     # Traverse to each adjacent node of a node
-    for child in graph[node]:
-        if not visited[child]:  # Check whether the node is visited or not
-            dfs(child, graph, visited, component)  # Call the dfs recursively
-
+    if node in graph:
+        for child in graph[node]:
+            if not visited[child]:  # Check whether the node is visited or not
+                dfs(child, graph, visited, component)  # Call the dfs recursively
 
 if __name__ == "__main__":
+    graph_file_path = "src/graph-test-100.txt"
 
-    graph = {
-        0: [2],
-        1: [2, 3],
-        2: [0, 1, 4],
-        3: [1, 4],
-        4: [2, 3]
-    }
-    node = 4  # root node
-    visited = [False]*len(graph)  # Make all nodes to False initially
+    graph = {}
+    with open(graph_file_path, 'r') as file:
+        next(file)  # Skip the first line
+        for line in file:
+            parts = list(map(int, line.split()))
+            node = parts[0]
+            neighbors = parts[1:]
+            graph[node] = neighbors
+
+    node = 2  # root node
+    visited = {key: False for key in graph.keys()}  # Initialize visited dictionary
     component = []
     dfs(node, graph, visited, component)
-    print(f"Following is the Depth-first search:{component}")
-    
-    print(type(component))
+    print(f"Following is the Depth-first search: {component}")  # Print the answer
